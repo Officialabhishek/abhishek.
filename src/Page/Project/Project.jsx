be26cd8b36bfe6065b817d13aa797bcd47104fd1
Abhishek Nagar <abhisheknagar0110@gmail.com>
2023-08-11 15:39:10 +0530

    feat: new project cards

--- src/Page/Project/Project.jsx
+++ src/Page/Project/Project.jsx
@@ -6,6 +6,8 @@ import Fade from 'react-reveal/Fade';
 import imgbmi from "../../assets/imgbmi.jpeg";
 import imgtodo from "../../assets/imgtodo.jpeg";
 import imgcalc from "../../assets/imgcalc.jpeg";
+import abhishek from "../../assets/abhishek.jpeg";
+import kapde from "../../assets/kapde.jpeg";
 
 const Project = () => {
     return (
@@ -13,6 +15,24 @@ const Project = () => {
             <Fade top><h1 className='heading'>My <span className='headspan'>Projects</span></h1></Fade>
             <div className='wrapper'>
                 <div className='row'>
+                    <div className='col'>
+                        <ProjectCard
+                            img={kapde}
+                            title="Kapde (E-Commerce Website)"
+                            desc="Kapde is a MERN e-commerce website. Backend part is not done yet. Soon available for demo"
+                            ghlink="https://github.com/Officialabhishek/e-comm"
+                            demolink="#"
+                        />
+                    </div>
+                    <div className='col'>
+                        <ProjectCard
+                            img={abhishek}
+                            title="abhishek."
+                            desc="abhishek. is my self coded personal portfolio website build using Reactjs and sass is used for styling. It is fully responsive in nature."
+                            ghlink="https://github.com/Officialabhishek/abhishek."
+                            demolink="https://abhisheknagar.vercel.app/"
+                        />
+                    </div>
                     <div className='col'>
                         <ProjectCard
                             img={imgtodo}
